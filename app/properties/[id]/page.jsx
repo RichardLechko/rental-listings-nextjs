f0a379915677c7c54078255b1107242d79f29e8f
RichardLechko <richardlechko04@gmail.com>
2024-10-26 22:07:56 -0500

    client components

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,35 @@
+"use client";
+
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from "next/navigation";
+
 const PropertyPage = () => {
-  return <div>Property Page</div>;
+  const router = useRouter();
+  const params = useParams();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  return (
+    <div>
+      {/* <button
+        className="block bg-blue-100 p-2"
+        onClick={() => router.replace("/")}
+      >
+        Back to Home
+      </button> */}
+
+      {/* use Params */}
+      {/* <div>Property Page {params.id}</div> */}
+
+      {/* searchParams */}
+      {/* <div>Property Page {searchParams.get("name")}</div> */}
+
+      <div>Property Page {pathname}</div>
+    </div>
+  );
 };
 
 export default PropertyPage;
